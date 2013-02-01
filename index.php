0acf07ab244eb9f67e4126f5ca98d7be4ab1262b
JD Hartley <jdhrtl@gmail.com>
2013-01-31 21:02:50 -0600

    Update styling

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!doctype html>
+<html>
+	<head>
+		<title>StageBloc Local Theme Development</title>
 
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+		<script src="assets/js/jquery.cookie.min.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/bender/js/bender.js"></script>
+
+		<link rel="stylesheet" href="assets/bender/css/bender.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+
+	<body>
+<?php
 $loginRequired = false;
 
 // Include the StageBloc library
@@ -9,7 +23,7 @@ require_once 'php-stagebloc-api/StageBloc.php';
 if ( file_exists('config.php') )
 {
 	require_once 'config.php';
-	
+
 	if ( strpos($accessToken, ' ') !== false || empty($accessToken) )
 	{
 		$loginRequired = true;
@@ -28,7 +42,7 @@ if ( ! empty($_POST) ) // The user is attempting to login
 			'password' => $_POST['password']
 		);
 		$response = $stagebloc->post('oauth2/token/index', $params);
-		
+
 		$response = json_decode($response, true);
 
 		// Put this access token in our config file to make requests with
@@ -81,21 +95,6 @@ if ( ! $loginRequired )
 	$themeOptionsHTML .= '</select>';
 }
 ?>
-
-<html>
-	<head>
-		<title>StageBloc Local Theme Development</title>
-
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-		<script src="assets/js/jquery.cookie.min.js"></script>
-		<script src="assets/js/main.js"></script>
-		<script src="assets/bender/js/bender.js"></script>
-				
-		<link rel="stylesheet" type="text/css" href="assets/bender/css/bender.css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
-	</head>
-
-	<body>
 		<?php if ( ! file_exists('config.php') ): ?>
 			<div class="content">
 				<article>
@@ -104,15 +103,17 @@ if ( ! $loginRequired )
 				</article>
 			</div>
 		<?php elseif ( $loginRequired ): ?>
-			<header><h1><a href="" id="stagebloc-logo">StageBloc</a></h1></header>
-			
-			<form method="post" id="contact-form" autocomplete="off">
-				<fieldset>
-					<input class="input" type="text" id="email" name="email" placeholder="Email" required />
-					<input class="input" type="password" id="password" name="password" placeholder="Password" required />
-					<button type="submit" name="submit">Connect</button>
-				</fieldset>
-			</form>
+			<div class="content">
+				<header><h1><a href="" id="stagebloc-logo">StageBloc</a></h1></header>
+
+				<form method="post" autocomplete="off">
+					<fieldset>
+						<input class="input" type="text" id="email" name="email" placeholder="Email" required />
+						<input class="input" type="password" id="password" name="password" placeholder="Password" required />
+						<button type="submit" name="submit">Connect with StageBloc</button>
+					</fieldset>
+				</form>
+			</div>
 		<?php else: ?>
 			<div id="console">
 				<a href="http://stagebloc.com/developers/theming" target="_blank" class="docs"><i></i>Theming Engine Documentation &rarr;</a>
