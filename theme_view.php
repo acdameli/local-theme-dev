6c26b006daf6868ac550df81b6644221a319433e
JD Hartley <jdhrtl@gmail.com>
2015-07-15 13:51:00 -0500

    support url-replacing on action attribute

--- theme_view.php
+++ theme_view.php
@@ -116,9 +116,9 @@ try
 	if ( $customDomain )
 	{
 		// If it's a custom domain, the replacement is a little bit different
-		$renderedTheme = preg_replace('#href="(http:\/\/(?:www\.)?' . $customDomain . '\/?)#', 'href="?url=', $renderedTheme);
+		$renderedTheme = preg_replace('#(href|action)="(http:\/\/(?:www\.)?' . $customDomain . '\/?)#', '$1="?url=', $renderedTheme);
 	}
-	$renderedTheme = preg_replace('#href="((http:\/\/stagebloc\..+)?\/' . $accountUrl . '\/)#', 'href="?url=', $renderedTheme);
+	$renderedTheme = preg_replace('#(href|action)="((http:\/\/stagebloc\..+)?\/' . $accountUrl . '\/)#', '$1="?url=', $renderedTheme);
 
 	echo $renderedTheme; // Output the final result
 }
